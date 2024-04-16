08e6c51e3bfb744462e64a51307096c8965acffe
Alessandro <a.peria@student.uw.edu.pl>
2024-04-16 17:39:21 +0200

    Enable processing multiple videos with single model loading

--- inference.py
+++ inference.py
@@ -22,11 +22,17 @@ parser.add_argument('--checkpoint_path', type=str,
                     help='Name of saved checkpoint to load weights from', required=True)
 
 parser.add_argument('--face', type=str, 
-                    help='Filepath of video/image that contains faces to use', required=True)
+                    help='Filepath of video/image that contains faces to use',
+                    nargs="+",
+                    required=True)
 parser.add_argument('--audio', type=str, 
-                    help='Filepath of video/audio file to use as raw audio source', required=True)
+                    help='Filepath of video/audio file to use as raw audio source', 
+                    nargs="+",
+                    required=True)
+
 parser.add_argument('--outfile', type=str, help='Video path to save result. See default for an e.g.', 
-                                default='results/result_voice.mp4')
+                                default='results/result_voice.mp4',
+                    nargs="+")
 
 parser.add_argument('--static', type=bool, 
                     help='If True, then use only first video frame for inference', default=False)
@@ -172,119 +178,124 @@ def load_model(path):
 def main():
     args.img_size = 96
 
-    if os.path.isfile(args.face) and args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
-        args.static = True
+    for video_index in range(0, len(args.face)):
+        face = args.face[video_index]
+        audio = args.audio[video_index]
+        outfile = args.outfile[video_index]
+        
+        if os.path.isfile(face) and face.split('.')[1] in ['jpg', 'png', 'jpeg']:
+            args.static = True
 
-    if not os.path.isfile(args.face):
-        raise ValueError('--face argument must be a valid path to video/image file')
+        if not os.path.isfile(face):
+            raise ValueError('--face argument must be a valid path to video/image file')
 
-    elif args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
-        full_frames = [cv2.imread(args.face)]
-        fps = args.fps
+        elif face.split('.')[1] in ['jpg', 'png', 'jpeg']:
+            full_frames = [cv2.imread(face)]
+            fps = args.fps
 
-    else:
-        video_stream = cv2.VideoCapture(args.face)
-        fps = video_stream.get(cv2.CAP_PROP_FPS)
+        else:
+            video_stream = cv2.VideoCapture(face)
+            fps = video_stream.get(cv2.CAP_PROP_FPS)
 
-        print('Reading video frames...')
+            print('Reading video frames...')
 
-        full_frames = []
+            full_frames = []
+            while 1:
+                still_reading, frame = video_stream.read()
+                if not still_reading:
+                    video_stream.release()
+                    break
+
+                aspect_ratio = frame.shape[1] / frame.shape[0]
+                frame = cv2.resize(frame, (int(args.out_height * aspect_ratio), args.out_height))
+                # if args.resize_factor > 1:
+                #     frame = cv2.resize(frame, (frame.shape[1]//args.resize_factor, frame.shape[0]//args.resize_factor))
+
+                if args.rotate:
+                    frame = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
+
+                y1, y2, x1, x2 = args.crop
+                if x2 == -1: x2 = frame.shape[1]
+                if y2 == -1: y2 = frame.shape[0]
+
+                frame = frame[y1:y2, x1:x2]
+
+                full_frames.append(frame)
+
+        print ("Number of frames available for inference: "+str(len(full_frames)))
+
+        if not audio.endswith('.wav'):
+            print('Extracting raw audio...')
+            # command = 'ffmpeg -y -i {} -strict -2 {}'.format(audio, 'temp/temp.wav')
+            # subprocess.call(command, shell=True)
+            subprocess.check_call([
+                "ffmpeg", "-y",
+                "-i", audio,
+                "temp/temp.wav",
+            ])
+            audio = 'temp/temp.wav'
+
+        wav = audio.load_wav(audio, 16000)
+        mel = audio.melspectrogram(wav)
+        print(mel.shape)
+
+        if np.isnan(mel.reshape(-1)).sum() > 0:
+            raise ValueError('Mel contains nan! Using a TTS voice? Add a small epsilon noise to the wav file and try again')
+
+        mel_chunks = []
+        mel_idx_multiplier = 80./fps
+        i = 0
         while 1:
-            still_reading, frame = video_stream.read()
-            if not still_reading:
-                video_stream.release()
+            start_idx = int(i * mel_idx_multiplier)
+            if start_idx + mel_step_size > len(mel[0]):
+                mel_chunks.append(mel[:, len(mel[0]) - mel_step_size:])
                 break
+            mel_chunks.append(mel[:, start_idx : start_idx + mel_step_size])
+            i += 1
 
-            aspect_ratio = frame.shape[1] / frame.shape[0]
-            frame = cv2.resize(frame, (int(args.out_height * aspect_ratio), args.out_height))
-            # if args.resize_factor > 1:
-            #     frame = cv2.resize(frame, (frame.shape[1]//args.resize_factor, frame.shape[0]//args.resize_factor))
+        print("Length of mel chunks: {}".format(len(mel_chunks)))
 
-            if args.rotate:
-                frame = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
+        full_frames = full_frames[:len(mel_chunks)]
 
-            y1, y2, x1, x2 = args.crop
-            if x2 == -1: x2 = frame.shape[1]
-            if y2 == -1: y2 = frame.shape[0]
+        batch_size = args.wav2lip_batch_size
+        gen = datagen(full_frames.copy(), mel_chunks)
 
-            frame = frame[y1:y2, x1:x2]
+        s = time()
 
-            full_frames.append(frame)
+        for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen,
+                                                total=int(np.ceil(float(len(mel_chunks))/batch_size)))):
+            if i == 0:
+                frame_h, frame_w = full_frames[0].shape[:-1]
+                out = cv2.VideoWriter('temp/result.avi',
+                                        cv2.VideoWriter_fourcc(*'DIVX'), fps, (frame_w, frame_h))
 
-    print ("Number of frames available for inference: "+str(len(full_frames)))
+            img_batch = torch.FloatTensor(np.transpose(img_batch, (0, 3, 1, 2))).to(device)
+            mel_batch = torch.FloatTensor(np.transpose(mel_batch, (0, 3, 1, 2))).to(device)
 
-    if not args.audio.endswith('.wav'):
-        print('Extracting raw audio...')
-        # command = 'ffmpeg -y -i {} -strict -2 {}'.format(args.audio, 'temp/temp.wav')
-        # subprocess.call(command, shell=True)
+            with torch.no_grad():
+                pred = model(mel_batch, img_batch)
+
+            pred = pred.cpu().numpy().transpose(0, 2, 3, 1) * 255.
+
+            for p, f, c in zip(pred, frames, coords):
+                y1, y2, x1, x2 = c
+                p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
+
+                f[y1:y2, x1:x2] = p
+                out.write(f)
+
+        out.release()
+
+        print("wav2lip prediction time:", time() - s)
+
         subprocess.check_call([
             "ffmpeg", "-y",
-            "-i", args.audio,
-            "temp/temp.wav",
+            # "-vsync", "0", "-hwaccel", "cuda", "-hwaccel_output_format", "cuda",
+            "-i", "temp/result.avi",
+            "-i", audio,
+            # "-c:v", "h264_nvenc",
+            outfile,
         ])
-        args.audio = 'temp/temp.wav'
-
-    wav = audio.load_wav(args.audio, 16000)
-    mel = audio.melspectrogram(wav)
-    print(mel.shape)
-
-    if np.isnan(mel.reshape(-1)).sum() > 0:
-        raise ValueError('Mel contains nan! Using a TTS voice? Add a small epsilon noise to the wav file and try again')
-
-    mel_chunks = []
-    mel_idx_multiplier = 80./fps
-    i = 0
-    while 1:
-        start_idx = int(i * mel_idx_multiplier)
-        if start_idx + mel_step_size > len(mel[0]):
-            mel_chunks.append(mel[:, len(mel[0]) - mel_step_size:])
-            break
-        mel_chunks.append(mel[:, start_idx : start_idx + mel_step_size])
-        i += 1
-
-    print("Length of mel chunks: {}".format(len(mel_chunks)))
-
-    full_frames = full_frames[:len(mel_chunks)]
-
-    batch_size = args.wav2lip_batch_size
-    gen = datagen(full_frames.copy(), mel_chunks)
-
-    s = time()
-
-    for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen,
-                                            total=int(np.ceil(float(len(mel_chunks))/batch_size)))):
-        if i == 0:
-            frame_h, frame_w = full_frames[0].shape[:-1]
-            out = cv2.VideoWriter('temp/result.avi',
-                                    cv2.VideoWriter_fourcc(*'DIVX'), fps, (frame_w, frame_h))
-
-        img_batch = torch.FloatTensor(np.transpose(img_batch, (0, 3, 1, 2))).to(device)
-        mel_batch = torch.FloatTensor(np.transpose(mel_batch, (0, 3, 1, 2))).to(device)
-
-        with torch.no_grad():
-            pred = model(mel_batch, img_batch)
-
-        pred = pred.cpu().numpy().transpose(0, 2, 3, 1) * 255.
-
-        for p, f, c in zip(pred, frames, coords):
-            y1, y2, x1, x2 = c
-            p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
-
-            f[y1:y2, x1:x2] = p
-            out.write(f)
-
-    out.release()
-
-    print("wav2lip prediction time:", time() - s)
-
-    subprocess.check_call([
-        "ffmpeg", "-y",
-        # "-vsync", "0", "-hwaccel", "cuda", "-hwaccel_output_format", "cuda",
-        "-i", "temp/result.avi",
-        "-i", args.audio,
-        # "-c:v", "h264_nvenc",
-        args.outfile,
-    ])
 
 model = detector = detector_model = None
 
@@ -316,6 +327,14 @@ def face_rect(images):
                 prev_ret = tuple(map(int, box))
             yield prev_ret
 
+def assert_same_length_input_output_args():
+    if len(args.audio) != len(args.outfile) or len(args.audio) != len(args.face):
+        parser.error("The number of --input and --output arguments must be the same.")
+
+def parse_args():
+    args = parser.parse_args()
+    assert_same_length_input_output_args()
+    return args
 
 if __name__ == '__main__':
     args = parser.parse_args()
